bcdd09c2854836b75e52f56c806e0a33897418eb
rajendra@datastra.com <gajendra.krishna@datastra.com>
2014-10-02 11:56:28 +0530

    Revert "Team Handoff"
    This reverts commit e88ef6d5a256cf4d4f469c58192d46774bfc266e.

--- bwssb_imr/config/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-#Database Connection Here....
-$dbc = mysqli_connect('localhost', 'bwssb_imr', 'bwssb_imr', 'bwssb_imr') OR die('Could not connect because '.mysqli_connect_error());
-date_default_timezone_set("Asia/Calcutta");
-?>
--- bwssb_ufw/config/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-#Database Connection Here....
-$dbc = mysqli_connect('localhost', 'bwssb', 'bwssb', 'bwssb_ufw') OR die('Could not connect because '.mysqli_connect_error());
-date_default_timezone_set("Asia/Calcutta");
-?>
